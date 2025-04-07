2aab1c33b88e751307f5ddbdd4e69d39b65d0820
Gayathri Nanda P T <134200322+gayathriinanda@users.noreply.github.com>
2025-04-07 20:40:21 +0530

    Add files via upload

---
+++ 3TierArchitectureApp/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cxaiimkuomma.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Gaya3456#',
+    DB_DATABASE : 'webappdb'
+});
